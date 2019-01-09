a1115157af628c971172483432de47dddbadb96c
unknown <d00349668@dmail.dixie.com>
2019-01-08 23:01:10 -0800

    added another APi test scenario

--- src/tests/API.test.js
+++ src/tests/API.test.js
@@ -4,10 +4,12 @@ test('handles repository requests', async () => {
   expect(await getRepos('facebook'))
   expect(await getRepos('google'))
   expect(await getRepos('cyrusdean'))
+  expect(await getRepos('ehsrthbndxhjdrtjass'))
 })
 
 test('handles user requests', async () => {
   expect(await getUserData('facebook'))
   expect(await getUserData('google'))
   expect(await getUserData('cyrusdean'))
+  expect(await getUserData('ehnjrsjdeshnedrsbhedawe'))
 })
